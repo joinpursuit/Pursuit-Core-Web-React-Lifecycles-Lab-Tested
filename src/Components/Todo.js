f74244aa293bc96cc39cf4bafee130c8ee96384a
Jenna Qian <jiayin1996@gmail.com>
2021-04-28 19:34:45 -0400

    Pass two test

---
+++ src/Components/Todo.js
@@ -0,0 +1,28 @@
+
+import React, { Component } from 'react'
+import {toast}  from "react-toastify"
+
+export class todo extends Component {
+    
+    componentDidMount () {
+        // const {todo} = this.props
+        toast.success(`New todo added: ${this.props.todo}`)
+    }
+
+    componentWillUnmount () {
+        // const {todo} = this.props
+        toast.error(`Todo deleted: ${this.props.removed}`)
+    }
+
+    render() {
+        const {todo,handleDelete} = this.props
+        return (
+            <div>
+                {todo}
+                <button onClick={handleDelete}>X</button>
+            </div>
+        )
+    }
+}
+
+export default todo;
