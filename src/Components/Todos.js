1ec9481ca44d96f68ada306230b6e1fab21bb2f1
anasnahil <nahil.anas1@gmail.com>
2021-04-30 17:24:41 -0400

    finished lab

---
+++ src/Components/Todos.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+
+class Todos extends React.Component {
+    componentDidMount() {
+        toast.success(`New todo added: ${this.props.todo.name}`)
+    }
+    
+
+    componentWillUnmount() {
+		toast.error(`Todo deleted: ${this.props.todo.name}`);
+	}
+    render() {
+        const { todo, removeTodo } = this.props;
+        return( 
+            <div>
+                <li>{todo.name}</li>
+                <button onClick={() => removeTodo(todo.name)}>x</button>
+            </div>
+        )
+    }
+}
+
+export default Todos;
