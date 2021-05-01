ec95bf323dae337b9e0c7298e681e4cde7c3e99c
Miguel Catala <miguelcatala27@yahoo.com>
2021-04-30 20:42:55 -0400

    lab done

---
+++ src/Todo.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { toast } from 'react-toastify'
+
+
+class Todo extends React.Component {
+    componentDidMount() {
+        toast(`New todo added: ${this.props.children}`, { type: "success" });
+      }
+    
+      componentWillUnmount() {
+        toast(`Todo deleted: ${this.props.children}`, { type: "error" });
+      }
+    
+      render() {
+        return (
+          <div>
+            {this.props.children}
+            <button onClick={this.props.onClose}>x</button>
+          </div>
+        );
+      }
+    }
+
+
+
+
+
+
+export default Todo
