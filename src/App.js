d5929e94459bfb9c6eaad2303f4408f041cd8d4f
Kathy Gee <mzchick@gmail.com>
2021-05-03 18:05:16 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,69 @@
 import React from "react";
 import { ToastContainer } from "react-toastify";
+import Todo from './Todo.js'
+import { v4 as uuid} from 'uuid' 
 
 import "../node_modules/react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      newTodo: "",
+      todos: [],
+    }
+  }
+
+  
+  handleChange = (e) => {
+    console.log(e.target.value)
+    this.setState({
+      newTodo: e.target.value
+    })
+
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const todo = {
+      id : {uuid},
+      text: this.state.newTodo,
+    }
+
+    this.setState({
+      todos: [...this.state.todos, todo],
+      newTodo: "",
+    })
+  }
+
+  remove = (id) => {
+    this.setState ({
+      todos: this.state.todos.filter((todo) => todo.id !== id),
+    })
+  }
+
+  reset = () => {
+    this.setState(() => this.initialState)
+  }
+
+
   render() {
+    const newTodo = this.state.newTodo
+    const todos = this.state.todos
+
+
     return (
       <div className="app">
         <ToastContainer />
-        Give me something to do?!
+        <form action="" onSubmit={this.handleSubmit}>
+          <input type="text" value={newTodo} onChange={this.handleChange} />
+        </form>
+        <ul id="todos">
+          {todos.map(todo => {
+            return <Todo key={todo.id} todo={todo} remove={this.remove}/>
+          })}
+        </ul>
       </div>
     );
   }
---
+++ src/Todo.js
@@ -0,0 +1,19 @@
+import {Component} from 'react'
+import { toast } from 'react-toastify'
+
+class Todo extends Component {
+    componentDidMount(){
+        toast.success(`New todo added: ${this.props.todo.text}`)
+    }
+
+render () {
+    const { todo, remove } = this.props
+    
+    return <div>
+        <p>{todo.text}</p>
+        <button onClick={() => remove(todo.id)}>x</button>
+    </div>
+    }
+}
+
+export default Todo
