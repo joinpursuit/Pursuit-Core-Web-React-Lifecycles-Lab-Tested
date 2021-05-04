17bb622462d7affba516b0da5ded6dee0c30b16a
JDJ97 <jdj97@techie.com>
2021-05-04 17:26:10 -0400

    Finished Assignment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from "react";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
+import Form from "./Components/Form";
+import Todos from "./Components/Todos";
 
 import "../node_modules/react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 class App extends React.Component {
+  state = { list: [] };
+
+  addToDo = (todo) => {
+    const Todo = { name: todo };
+    this.setState((prevState) => ({
+      list: [...prevState.list, Todo],
+    }));
+  };
+
+  removeTodo = (name) => {
+    const removeArr = this.state.list.filter((todo) => todo.name !== name);
+    this.setState({ list: removeArr });
+  };
+
   render() {
     return (
       <div className="app">
         <ToastContainer />
-        Give me something to do?!
+        <Form addToDo={this.addToDo} />
+        <ul id="todos">
+          {this.state.list.map((todo) => (
+            <Todos todo={todo} removeTodo={this.removeTodo} key={todo.name} />
+          ))}
+        </ul>
       </div>
     );
   }
