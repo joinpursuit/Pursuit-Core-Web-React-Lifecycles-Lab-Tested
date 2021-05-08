fb24bdf2a2dfaee38cedd15b43d40445395f754e
Jonathan Bedon <bedonjonathan@gmail.com>
2021-05-08 17:48:14 -0400

    passed all tests but bonus

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -1,28 +1,52 @@
-import React from "react";
-import { toast } from 'react-toastify';
+import React, { useEffect } from "react";
+import { toast } from "react-toastify";
 
-class ToDo extends React.Component {
+const ToDo = ({ todo, handleDeleteListItem }) => {
+  
+  
+  useEffect(() => {
+    toast.success(`New todo added: ${todo}`)
+    return () => {
+      toast.error(`Todo deleted: ${todo}`);
+    };
+  },[todo]);
 
-    componentDidMount(){
-        const { todo } = this.props;
-            toast(`New todo added: ${todo}`)   
-    }
-    componentWillUnmount(){
-        const { todo } = this.props;
-            toast(`${todo}`)
-    }
-  render() {
-    const { todo, handleDeleteListItem } = this.props;
-    return (
-     
-              <li id="todos">
-                {todo}{" "}
-                <button name={todo} onClick={handleDeleteListItem}>
-                  -
-                </button>{" "}
-              </li>
-    );
-  }
-}
+
+
+  
+
+  return (
+    <li>
+      {todo}{" "}
+      <button name={todo} onClick={handleDeleteListItem}>
+        -
+      </button>{" "}
+    </li>
+  );
+};
+
+// class ToDo extends React.Component {
+
+//     componentDidMount(){
+//         const { todo } = this.props;
+//             toast(`New todo added: ${todo}`)
+//     }
+//     componentWillUnmount(){
+//         const { todo } = this.props;
+//             toast(`${todo}`)
+//     }
+//   render() {
+//     const { todo, handleDeleteListItem } = this.props;
+//     return (
+
+//               <li id="todos">
+//                 {todo}{" "}
+//                 <button name={todo} onClick={handleDeleteListItem}>
+//                   -
+//                 </button>{" "}
+//               </li>
+//     );
+//   }
+// }
 
 export default ToDo;
--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -1,55 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
 import ToDo from "./ToDo";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-toastify";
 
-class ToDoList extends React.Component {
-  state = { input: "", listOfToDos: [] };
+let id = 0
+const ToDoList = () => {
+  const [input, setInput] = useState("");
+  const [listOfToDos, setListOfTodos] = useState([]);
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
+    // debugger
     e.preventDefault();
-    this.setState((prevState) => {
-      return { listOfToDos: [this.state.input, ...prevState.listOfToDos] };
-    });
-    this.setState({ input: "" });
+    setListOfTodos([...listOfToDos,input]);
+    // toast.success(`New todo added: ${input}`)
+    setInput("");
   };
 
-  handleInput = (e) => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
+  const handleInput = (e) => {
+    const { value } = e.target;
+    setInput(value);
+    // debugger
   };
 
-  handleDeleteListItem = (e) => {
+  const handleDeleteListItem = (e) => {
     const { name } = e.target;
-    const { listOfToDos } = this.state;
-    this.setState({
-      listOfToDos: listOfToDos.filter((todo) => {
+    // toast.error(`Todo deleted: ${name}`);
+
+    setListOfTodos(
+      listOfToDos.filter((todo) => {
         return todo !== name;
-      }),
-    });
+      })
+    );
+    // debugger
   };
 
- 
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <input value={input} onChange={handleInput} />
+      </form>
+      <ul id="todos">
+      {listOfToDos.map((todo) => {
+        return (
+          <ToDo
+            key={id + 1}
+            todo={todo}
+            handleDeleteListItem={handleDeleteListItem}
+          />
+        );
+      })}
+      </ul>
+    </section>
+  );
+};
 
-  render() {
-    const { input, listOfToDos} = this.state;
-    return (
-      <section>
-        <form onSubmit={this.handleSubmit}>
-          <input name="input" value={input} onChange={this.handleInput} />
-        </form>
-        {listOfToDos.map((todo) => {
-            return (
-              <ToDo
-                key={todo}
-                todo={todo}
-                handleDeleteListItem={this.handleDeleteListItem}
-              />
-            );
-          })}
+// class ToDoList extends React.Component {
+//   state = { input: "", listOfToDos: [] };
 
-      </section>
-    );
-  }
-}
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+//     this.setState((prevState) => {
+//       return { listOfToDos: [this.state.input, ...prevState.listOfToDos] };
+//     });
+//     this.setState({ input: "" });
+//   };
+
+//   handleInput = (e) => {
+//     const { name, value } = e.target;
+//     this.setState({ [name]: value });
+//   };
+
+//   handleDeleteListItem = (e) => {
+//     const { name } = e.target;
+//     const { listOfToDos } = this.state;
+//     this.setState({
+//       listOfToDos: listOfToDos.filter((todo) => {
+//         return todo !== name;
+//       }),
+//     });
+//   };
+
+//   render() {
+//     const { input, listOfToDos} = this.state;
+//     return (
+//       <section>
+//         <form onSubmit={this.handleSubmit}>
+//           <input name="input" value={input} onChange={this.handleInput} />
+//         </form>
+//         {listOfToDos.map((todo) => {
+//             return (
+//               <ToDo
+//                 key={todo}
+//                 todo={todo}
+//                 handleDeleteListItem={this.handleDeleteListItem}
+//               />
+//             );
+//           })}
+
+//       </section>
+//     );
+//   }
+// }
 
 export default ToDoList;
