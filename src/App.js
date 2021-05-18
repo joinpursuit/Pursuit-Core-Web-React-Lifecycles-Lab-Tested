3111372a9b3de1cc4a2481f471403ada4d5496f1
Hady Mohamed <hadymohamed@pursuit.org>
2021-05-18 02:24:48 -0400

    refactored lab using usestate and useeffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,56 @@
-import React from "react";
+// import React from "react";
+// import { ToastContainer } from "react-toastify";
+// import Form from "./Components/Form";
+// import TodoList from "./Components/TodoList";
+
+// import "../node_modules/react-toastify/dist/ReactToastify.css";
+// import "./App.css";
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       todo: "",
+//       todoList: [],
+//     };
+//   }
+
+//   handleInput = (event) => {
+//     this.setState({ todo: event.target.value });
+//   };
+
+//   handleDisplayList = (event) => {
+//     event.preventDefault();
+//     this.setState({ todoList: this.state.todoList.concat(this.state.todo) });
+//   };
+//   handleDelete = (i) => {
+//     const todoList = [...this.state.todoList];
+//     todoList.splice(i, 1);
+//     this.setState({ todoList });
+//   };
+
+//   render() {
+//     return (
+//       <div className="app">
+//         <Form
+//           handleInput={this.handleInput}
+//           handleDisplayList={this.handleDisplayList}
+//         />
+//         <TodoList
+//           handleDelete={this.handleDelete}
+//           todoList={this.state.todoList}
+//         />
+//         <ToastContainer />
+//         Give me something to do?!
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+import React, { useState } from "react";
 import { ToastContainer } from "react-toastify";
 import Form from "./Components/Form";
 import TodoList from "./Components/TodoList";
@@ -6,46 +58,31 @@ import TodoList from "./Components/TodoList";
 import "../node_modules/react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
-class App extends React.Component {
-  constructor() {
-    super();
+export default function App() {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
 
-    this.state = {
-      todo: "",
-      todoList: [],
-    };
+  function handleInput(event) {
+    setTodo(event.target.value);
   }
 
-  handleInput = (event) => {
-    this.setState({ todo: event.target.value });
-  };
-
-  handleDisplayList = (event) => {
+  function handleDisplayList(event) {
     event.preventDefault();
-    this.setState({ todoList: this.state.todoList.concat(this.state.todo) });
-  };
-  handleDelete = (i) => {
-    const todoList = [...this.state.todoList];
-    todoList.splice(i, 1);
-    this.setState({ todoList });
-  };
-
-  render() {
-    return (
-      <div className="app">
-        <Form
-          handleInput={this.handleInput}
-          handleDisplayList={this.handleDisplayList}
-        />
-        <TodoList
-          handleDelete={this.handleDelete}
-          todoList={this.state.todoList}
-        />
-        <ToastContainer />
-        Give me something to do?!
-      </div>
-    );
+    setTodoList(todoList.concat(todo));
   }
-}
 
-export default App;
+  function handleDelete(i) {
+    const list = [...todoList];
+
+    setTodoList(list.splice((i, 1)));
+  }
+
+  return (
+    <div className="app">
+      <Form handleInput={handleInput} handleDisplayList={handleDisplayList} />
+      <TodoList handleDelete={handleDelete} todoList={todoList} />
+      <ToastContainer />
+      Give me something to do?!
+    </div>
+  );
+}
--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,24 +1,44 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+// import { toast } from "react-toastify";
+
+// export default class Todo extends Component {
+//   componentDidMount() {
+//     toast.success(`New todo added: ${this.props.todo}`);
+//   }
+
+//   componentWillUnmount() {
+//     toast.error(`Todo deleted: ${this.props.todo}`);
+//   }
+
+//   render() {
+//     const { todo, handleDelete, i } = this.props;
+//     return (
+//       <div>
+//         <li>
+//           {todo}
+//           <button onClick={() => handleDelete(i)}>-</button>
+//         </li>
+//       </div>
+//     );
+//   }
+// }
+
+import React, { useEffect } from "react";
 import { toast } from "react-toastify";
 
-export default class Todo extends Component {
-  componentDidMount() {
-    toast.success(`New todo added: ${this.props.todo}`);
-  }
-
-  componentWillUnmount() {
-    toast.error(`Todo deleted: ${this.props.todo}`);
-  }
-
-  render() {
-    const { todo, handleDelete, i } = this.props;
-    return (
-      <div>
-        <li>
-          {todo}
-          <button onClick={() => handleDelete(i)}>-</button>
-        </li>
-      </div>
-    );
-  }
+export default function Todo(props) {
+  useEffect(() => {
+    toast.success(`New todo added: ${props.todo}`);
+    return () => {
+      toast.error(`Todo deleted: ${props.todo}`);
+    };
+  });
+  return (
+    <div>
+      <li>
+        {props.todo}
+        <button onClick={() => props.handleDelete(props.i)}>-</button>
+      </li>
+    </div>
+  );
 }
