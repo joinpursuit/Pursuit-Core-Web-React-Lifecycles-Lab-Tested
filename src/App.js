f43c3e069cd79690a38840f7a232258f8e063eaf
Raymond Udeogu <rfizzle25@gmail.com>
2021-06-04 09:52:11 -0400

    need to use toastify

--- src/App.js
+++ src/App.js
@@ -2,20 +2,44 @@ import React from "react";
 import { ToastContainer } from "react-toastify";
 import "../node_modules/react-toastify/dist/ReactToastify.css";
 import "./App.css";
-import Todo from "./ToDo"
+import ToDo from "./ToDo";
+
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
       todos: [],
+      userInput:"",
+      
     }
   }
+  makeTodo = (e) => {
+    const value = e.target.value
+    const userInput = value
+    this.setState ({userInput})
+  }
+
+  addToDo = (e) => {
+    //get current userInput and current todos array
+    //push userInput into todos array
+    //update todos and make userInput empty string
+    e.preventDefault()
+    const { userInput, todos } = this.state
+    todos.push(userInput)
+    this.setState({todos, userInput:""})
+  }
+
   render() {
+    const { userInput, todos } = this.state
     return (
       <div className="app">
         <ToastContainer />
         Give me something to do?!
+          <form onSubmit={this.addToDo}>
+          <input type="text" value={userInput} name="" onChange={this.makeTodo} id=""/>
+          <ToDo todos={todos} />
+        </form>
       </div>
     );
   }
--- src/ToDo.js
+++ src/ToDo.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
 
 export default class ToDo extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    renderList = (todos) => {
+       return todos.map((elem, i) =>{
+            return (
+                <li key={i}>{elem}</li>
+            )
+        })
+    }
     render() {
+        const { todos } = this.props
+        if (todos.length === 0) {
+            return null
+        }  
         return (
-            <div>
-                <form action=""></form>
-                <input type="text" value="Enter a ToDo"/>
-                
-            </div>
+            <ul>
+                {this.renderList(todos)}
+            </ul>
         )
     }
 }
